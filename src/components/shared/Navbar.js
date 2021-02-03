872a62dd4b798c8bf3b8c6473a41e2fa547a6c5b
Vanessa Stewart <vstewart1213@gmail.com>
2021-02-02 22:31:07 -0800

    Update navbar elements

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -10,14 +10,13 @@ const Navbar = () => {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse " id="navbarNavAltMarkup">
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-              <a className="nav-link" aria-current="page"  href="/">Home</a>
-              <a className="nav-link" href="/eat">Eat</a>
+              <a className="nav-link" aria-current="page" href="/">Home</a>
+              <a className="nav-link" href="/eat">Menu</a>
               <a className="nav-link" href="/gallery">Gallery</a>
               <a className="nav-link" href="/location">Location</a>
               <a className="nav-link" href="/team">Team</a>
-              <a className="nav-link" href="/about">About</a>
             </div>
           </div>
         </div>
