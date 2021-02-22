479177e6e74415507d07de9c2a372d2b799804d0
Vanessa Stewart <vstewart1213@gmail.com>
2021-02-21 17:00:38 -0800

    Update Gallery page so that all four carousels are working

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,129 +6,178 @@ const Gallery = () => {
       <div className="gallery">
         <h1>Gallery</h1>
 
-        <h2>Drinks</h2>
         <div className="gallery-drinks">
-          <div id="carouselDrinksIndicators" className="carousel slide" data-ride="carousel">
-            <ol className="carousel-indicators">
-              <li data-target="#carouselDrinksIndicators" data-slide-to="0" className="active"></li>
-              <li data-target="#carouselDrinksIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselDrinksIndicators" data-slide-to="2"></li>
-            </ol>
-            <div className="carousel-inner-drinks">
-              <div className="carousel-item active">
-                <img className="d-block w-100" src="/img/array-of-eight-drinks.jpeg" alt="First slide" />
-              </div>
-              <div className="carousel-item">
-                <img className="d-block w-100" src="/img/boba-bush-background.jpeg" alt="Second slide" />
-              </div>
-              <div className="carousel-item">
-                <img className="d-block w-100" src="/img/boba.jpeg" alt="Third slide" />
-              </div>
-            </div>
-            <a className="carousel-control-prev" href="#carouselDrinksIndicators" role="button" data-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="sr-only">Previous</span>
-            </a>
-            <a className="carousel-control-next" href="#carouselDrinksIndicators" role="button" data-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="sr-only">Next</span>
-            </a>
-          </div>
-        </div>
-
-
-        <h2 className='mt-5'>Entrees</h2>
-        {/* <div classNameName="gallery-entrees">
-        <div id="carouselExampleIndicatorsEntrees" class="carousel slide" data-ride="carousel">
-          <ol className="carousel-indicators">
-            <li data-target="#carouselExampleIndicatorsEntrees" data-slide-to="0" className="active"></li>
-            <li data-target="#carouselExampleIndicatorsEntrees" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicatorsEntrees" data-slide-to="2"></li>
-          </ol>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img className="d-block w-100" src="/img/balls.jpeg" alt="First slide" />
-            </div>
-            <div className="carousel-item">
-              <img className="d-block w-100" src="..." alt="Second slide" />
-            </div>
-            <div className="carousel-item">
-              <img className="d-block w-100" src="..." alt="Third slide" />
-            </div>
-          </div>
-          <a className="carousel-control-prev" href="#carouselExampleIndicatorsEntrees" role="button" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a className="carousel-control-next" href="#carouselExampleIndicatorsEntrees" role="button" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-          </a>
-        </div>
-      </div> */}
-
-
-        <h2 className='mt-5'>Sides</h2>
-        {/* <div className="gallery-sides">
-        <div id="carouselSidesIndicators" class="carousel slide" data-ride="carousel">
-              <ol className="carousel-indicators">
-                <li data-target="#carouselSidesIndicators" data-slide-to="0" className="active"></li>
-                <li data-target="#carouselSidesIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselSidesIndicators" data-slide-to="2"></li>
-              </ol>
+          <h2>Drinks</h2>
+          <div className="row justify-content-center">
+            <div id="carouselDrinks" className="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
               <div className="carousel-inner">
+
                 <div className="carousel-item active">
-                  <img className="d-block w-100" src="/img/Boba cake.jpg" alt="First slide" />
+                  <img src="/img/four-fruit-drinks.jpeg" className="d-block w-100" alt="" />
                 </div>
+
                 <div className="carousel-item">
-                  <img className="d-block w-100" src="..." alt="Second slide" />
+                  <img src="/img/hotpot1.jpg" className="d-block w-100" alt="..." />
                 </div>
+
                 <div className="carousel-item">
-                  <img className="d-block w-100" src="..." alt="Third slide" />
+                  <img src="/img/boba.jpeg" className="d-block w-100" alt="..." />
                 </div>
+
+                <div className="carousel-item">
+                  <img src="/img/buns.jpeg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/drinks-on-table.jpeg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/desert1.jpg" className="d-block w-100" alt="..." />
+                </div>
+
               </div>
-              <a className="carousel-control-prev" href="#carouselSidesIndicators" role="button" data-slide="prev">
+              <a className="carousel-control-prev" href="#carouselDrinks" role="button" data-bs-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
+                <span className="visually-hidden">Previous</span>
               </a>
-              <a className="carousel-control-next" href="#carouselSidesIndicators" role="button" data-slide="next">
+              <a className="carousel-control-next" href="#carouselDrinks" role="button" data-bs-slide="next">
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
+                <span className="visually-hidden">Next</span>
               </a>
-            </div>
+            </div> 
           </div>
- */}
+        </div>
 
-        <h2 className='mt-5'>Desserts</h2>
-        {/* <div className="gallery-desserts">
-        <div id="carouselDessertsIndicators" class="carousel slide" data-ride="carousel">
-            <ol className="carousel-indicators">
-              <li data-target="#carouselDessertsIndicators" data-slide-to="0" className="active"></li>
-              <li data-target="#carouselDessertsIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselDessertsIndicators" data-slide-to="2"></li>
-            </ol>
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <img className="d-block w-100" src="/img/Boba cake.jpg" alt="First slide" />
+        <div className="gallery-entrees">
+          <h2>Entrees</h2>
+          <div className="row justify-content-center">
+            <div id="carouselEntrees" className="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
+              <div className="carousel-inner">
+
+                <div className="carousel-item active">
+                  <img src="/img/four-fruit-drinks.jpeg" className="d-block w-100" alt="" />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/hotpot1.jpg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/boba.jpeg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/buns.jpeg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/drinks-on-table.jpeg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/desert1.jpg" className="d-block w-100" alt="..." />
+                </div>
+
               </div>
-              <div className="carousel-item">
-                <img className="d-block w-100" src="..." alt="Second slide" />
-              </div>
-              <div className="carousel-item">
-                <img className="d-block w-100" src="..." alt="Third slide" />
-              </div>
-            </div>
-            <a className="carousel-control-prev" href="#carouselDessertsIndicators" role="button" data-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="sr-only">Previous</span>
-            </a>
-            <a className="carousel-control-next" href="#carouselDessertsIndicators" role="button" data-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="sr-only">Next</span>
-            </a>
+              <a className="carousel-control-prev" href="#carouselEntrees" role="button" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </a>
+              <a className="carousel-control-next" href="#carouselEntrees" role="button" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </a>
+            </div> 
           </div>
-        </div> */}
+        </div>
 
+        <div className="gallery-sides">
+          <h2>Sides</h2>
+          <div className="row justify-content-center">
+            <div id="carouselSides" className="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
+              <div className="carousel-inner">
+
+                <div className="carousel-item active">
+                  <img src="/img/four-fruit-drinks.jpeg" className="d-block w-100" alt="" />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/hotpot1.jpg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/boba.jpeg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/buns.jpeg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/drinks-on-table.jpeg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/desert1.jpg" className="d-block w-100" alt="..." />
+                </div>
+
+              </div>
+              <a className="carousel-control-prev" href="#carouselSides" role="button" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </a>
+              <a className="carousel-control-next" href="#carouselSides" role="button" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </a>
+            </div> 
+          </div>
+        </div>
+
+        <div className="gallery-desserts">
+          <h2>Desserts</h2>
+          <div className="row justify-content-center">
+            <div id="carouselDesserts" className="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
+              <div className="carousel-inner">
+
+                <div className="carousel-item active">
+                  <img src="/img/four-fruit-drinks.jpeg" className="d-block w-100" alt="" />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/hotpot1.jpg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/boba.jpeg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/buns.jpeg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/drinks-on-table.jpeg" className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="carousel-item">
+                  <img src="/img/desert1.jpg" className="d-block w-100" alt="..." />
+                </div>
+
+              </div>
+              <a className="carousel-control-prev" href="#carouselDesserts" role="button" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </a>
+              <a className="carousel-control-next" href="#carouselDesserts" role="button" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </a>
+            </div> 
+          </div>
+        </div>
+        
 
       </div>
     </React.Fragment>
--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -12,42 +12,42 @@ const Header = () => {
 
         <div className="row justify-content-center">
           <div id="carouselExampleDark" className="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
-          <div className="carousel-inner">
+            <div className="carousel-inner">
 
-            <div className="carousel-item active" data-bs-interval="3000">
-              <img src="/img/four-fruit-drinks.jpeg" className="d-block w-100" alt="" />
+              <div className="carousel-item active" data-bs-interval="3000">
+                <img src="/img/four-fruit-drinks.jpeg" className="d-block w-100" alt="" />
+              </div>
+
+              <div className="carousel-item" data-bs-interval="3000">
+                <img src="/img/hotpot1.jpg" className="d-block w-100" alt="..." />
+              </div>
+
+              <div className="carousel-item" data-bs-interval="3000">
+                <img src="/img/boba.jpeg" className="d-block w-100" alt="..." />
+              </div>
+
+              <div className="carousel-item" data-bs-interval="3000">
+                <img src="/img/buns.jpeg" className="d-block w-100" alt="..." />
+              </div>
+
+              <div className="carousel-item" data-bs-interval="3000">
+                <img src="/img/drinks-on-table.jpeg" className="d-block w-100" alt="..." />
+              </div>
+
+              <div className="carousel-item" data-bs-interval="3000">
+                <img src="/img/desert1.jpg" className="d-block w-100" alt="..." />
+              </div>
+
             </div>
-
-            <div className="carousel-item" data-bs-interval="3000">
-              <img src="/img/hotpot1.jpg" className="d-block w-100" alt="..." />
-            </div>
-
-            <div className="carousel-item" data-bs-interval="3000">
-              <img src="/img/boba.jpeg" className="d-block w-100" alt="..." />
-            </div>
-
-            <div className="carousel-item" data-bs-interval="3000">
-              <img src="/img/buns.jpeg" className="d-block w-100" alt="..." />
-            </div>
-
-            <div className="carousel-item" data-bs-interval="3000">
-              <img src="/img/drinks-on-table.jpeg" className="d-block w-100" alt="..." />
-            </div>
-
-            <div className="carousel-item" data-bs-interval="3000">
-              <img src="/img/desert1.jpg" className="d-block w-100" alt="..." />
-            </div>
-
-          </div>
-          <a className="carousel-control-prev" href="#carouselExampleDark" role="button" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </a>
-          <a className="carousel-control-next" href="#carouselExampleDark" role="button" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </a>
-        </div> 
+            <a className="carousel-control-prev" href="#carouselExampleDark" role="button" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carouselExampleDark" role="button" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </a>
+          </div> 
         </div>
       </div>
     </React.Fragment>
