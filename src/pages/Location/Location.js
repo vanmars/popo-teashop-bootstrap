edffbce26fcd78549ef42af8301e8989f06a14c8
Vanessa Stewart <vstewart1213@gmail.com>
2021-05-11 14:20:14 -0700

    Begin adding responsive styling to location page

--- src/pages/Location/Location.js
+++ src/pages/Location/Location.js
@@ -15,46 +15,44 @@ const Location = () => {
       
       <div className="drive-thru">
         <h2 data-aos="fade-right">We are open for drive thru and curbside pickup!</h2>
-          <Row md={1} lg={3} className="justify-content-center pb-5">
+          <Row  md={1} lg={3} className="justify-content-center pb-5">
             <Col>
-              <img src="/img/curbside-pickup.jpeg" alt=""/>
+              <img src="/img/drive-thru.jpeg" alt=""/>
             </Col>
             <Col>
               <img src="/img/drive-thru-window-with-car.jpeg" alt=""/>
             </Col>
             <Col>
-              <img src="/img/drive-thru.jpeg" alt=""/>
+              <img src="/img/curbside-pickup.jpeg"  alt=""/>
             </Col>
           </Row>
-          <h3 className="text-right" data-aos="fade-right">Order delivery with <a href="https://www.doordash.com/store/popo-pullman-637614">DoorDash</a>.</h3>
+          <h3 data-aos="fade-right">Order delivery with <a href="https://www.doordash.com/store/popo-pullman-637614">DoorDash</a>.</h3>
       </div>
 
       <div className="location-image"></div>
 
-      <div className="inside-store text-center">
+      <div className="inside-store">
         <h2 data-aos="fade-right">We can't wait to dine with you soon!</h2>
-          <Row md={1} lg={3}>
-            <Col>
-              <img src="/img/cash-register-with-bar.jpeg" alt=""/>
-            </Col>
-            <Col>
-              <img src="/img/frontdoor-from-inside.jpeg" alt=""/>
-            </Col>
-            <Col>
-              <img src="/img/boardgames-with-drinks.jpeg" alt=""/>
-            </Col>
-          </Row>
-          <Row  md={1} lg={3}>
-            <Col>
-              <img src="/img/inside-store-2.jpeg" alt=""/>
-            </Col>
-            <Col>
-              <img src="/img/inside-store-3.jpeg" alt=""/>
-            </Col>
-            <Col>
-              <img src="/img/inside-store-4.jpeg" alt=""/>
-            </Col>
-          </Row>
+        <Row xs={3} md={1} lg={3} className="inside-store-row-container">
+          <Col>
+            <img src="/img/cash-register-with-bar.jpeg" alt=""/>
+          </Col>
+          <Col>
+            <img src="/img/frontdoor-from-inside.jpeg" alt=""/>
+          </Col>
+          <Col>
+            <img src="/img/boardgames-with-drinks.jpeg" alt=""/>
+          </Col>
+          <Col>
+            <img src="/img/inside-store-2.jpeg" alt=""/>
+          </Col>
+          <Col>
+            <img src="/img/inside-store-3.jpeg" alt=""/>
+          </Col>
+          <Col>
+            <img src="/img/inside-store-4.jpeg" alt=""/>
+          </Col>
+        </Row>
       </div>
 
       <div className="location-image"></div>
